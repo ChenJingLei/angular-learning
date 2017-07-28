1e82bba00ec61a068ce56deff73ab7a2e49eb725
John Chen <cjl4565@163.com>
2017-07-28 08:59:11 +0800

    finished routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a routerLink="/dashboard">Dashboard</a>
-            <a routerLink="/heroes">Heroes</a>
-        <nav>
-        <router-outlet></router-outlet>
-    `,
-    styles: [],
-    providers: [],
+  selector: 'app-root',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
+  providers: [],
 })
 
 export class AppComponent {
-    title = 'Tour of Heroes';
+  title = 'Tour of Heroes';
 }
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,30 +8,31 @@ import { HeroService } from './hero.service';
 import 'rxjs/add/operator/switchMap';
 
 @Component({
-    selector: 'app-hero-detail',
-    templateUrl: './hero-detail.component.html',
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
 
-    hero: Hero;
+  hero: Hero;
 
-    constructor(
-        private heroService: HeroService,
-        private route: ActivatedRoute,
-        private location: Location
-    ) { }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
-    ngOnInit(): void {
-        this.route.paramMap
-            // + is a Javascript operator which can convert string to number
-            .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
-            .subscribe(hero => this.hero = hero);
-    }
+  ngOnInit(): void {
+    this.route.paramMap
+      // + is a Javascript operator which can convert string to number
+      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+      .subscribe(hero => this.hero = hero);
+  }
 
-    goBack(): void {
-        this.location.back();
-    }
+  goBack(): void {
+    this.location.back();
+  }
 
 
 }
